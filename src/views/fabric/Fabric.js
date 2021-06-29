0976c8155ca3bf4c157256b4c4cafdb8935a4ea0
lixiong <346959251@qq.com>
2021-06-29 20:08:29 +0800

    update

--- src/views/fabric/Fabric.js
+++ src/views/fabric/Fabric.js
@@ -1,79 +1,122 @@
-import { useEffect, useRef, useState } from 'react'
-import { fabric } from 'fabric'
+import { useEffect, useRef, useState } from "react";
+import { fabric } from "fabric";
 import styled from "@emotion/styled";
-import { Button } from 'antd';
+import { Button, Radio, InputNumber } from "antd";
 
 const Box = styled.div`
-background: #eee;
-`
+  background: #eee;
+`;
 const Control = styled.div`
-margin: 6px 0;
-background: #ccc;
-padding: 4px 0;
-`
+  margin: 6px 0;
+  background: #ccc;
+  padding: 4px 10px;
+`;
 const Content = styled.div`
-background: #ddd;
-height: 300px;
-`
+  background: #ddd;
+  height: 300px;
+`;
 
-function Fabric () {
-  const [rect, setRect] = useState(null)
-  const [line, setLine] = useState(null)
-  const canvas = useRef()
+function Fabric() {
+  const [drawType, setDrawType] = useState(1);
+  const [tableCols, setTableCols] = useState(1);
+  const [rect, setRect] = useState(null);
+  const [line, setLine] = useState(null);
+  const canvas = useRef();
   const add = () => {
-    const r = new fabric.Rect({
-      noScaleCache: false,
+    const r2 = new fabric.Rect({
+      // 边框不缩放
       strokeUniform: true,
-      stroke: '#000',
+      stroke: "#000",
       strokeWidth: 4,
-      left: 50,//距离画布左侧的距离，单位是像素
-      top: 50,//距离画布上边的距离
-      fill: 'red',//填充的颜色
-      width: 100,//方形的宽度
-      height: 100 //方形的高度
+      left: 400, //距离画布左侧的距离，单位是像素
+      top: 50, //距离画布上边的距离
+      fill: "green", //填充的颜色
+      width: 5, //方形的宽度
+      height: 50, //方形的高度
+      // lockScalingX: true, // 禁用水平缩放
+      lockMovementY: true, // 禁用垂直移动
+      lockScalingFlip: true, // 锁定翻转
+      hasRotatingPoint: false,
+      hasControls: false, // 是否显示控制点
+      // hoverCursor: "pointer",
+    });
+
+    const r = new fabric.Rect({
+      strokeUniform: true,
+      stroke: "#000",
+      strokeWidth: 4,
+      left: 50, //距离画布左侧的距离，单位是像素
+      top: 50, //距离画布上边的距离
+      fill: "red", //填充的颜色
+      width: 100, //方形的宽度
+      height: 100, //方形的高度
+      lockScalingFlip: true,
+      // borderDashArray: [5],
     });
     const l = new fabric.Line([90, 50, 90, 150], {
-      stroke: 'green',
+      stroke: "green",
       strokeUniform: true,
-      strokeWidth: 8
+      strokeWidth: 2,
+      padding: 6,
       // cornerSize: 30
-    })
-    canvas.current.add(r)
-    canvas.current.add(l)
-    setRect(r)
-    setLine(l)
-  }
+    });
+    canvas.current.add(r);
+    canvas.current.add(r2);
+    canvas.current.add(l);
+    setRect(r);
+    setLine(l);
+  };
 
-  const move = () => {
-    rect.set({
-      left: 200
-    })
-    canvas.current.renderAll()
-  }
-
+  // const move = () => {
+  //   rect.set({
+  //     left: 200,
+  //   });
+  //   canvas.current.renderAll();
+  // };
 
   useEffect(() => {
-    // const canvas = new fabric.Canvas('main')
-    canvas.current = new fabric.Canvas('main', {
-      preserveObjectStacking: true
-    })
-  }, [])
+    const c = new fabric.Canvas("main", {
+      // 选中对象不会到最高层，按原层次摆放
+      preserveObjectStacking: true,
+    });
+    canvas.current = c;
+    c.on("mouse:down", ({ pointer, target }) => {
+      if (!target) {
+        // 新增
 
-
+        console.log(pointer, target);
+      }
+    });
+    c.clipTo = function (ctx) {
+      ctx.arc(100, 100, 200, 0, Math.PI*2, true);
+    }
+    add();
+  }, []);
 
   return (
     <Box>
       <Control>
-        <Button type="primary" onClick={add}>新增</Button>
-        <Button onClick={move}>移动</Button>
-        <Button type="dashed">删除</Button>
+        <Radio.Group
+          onChange={(e) => setDrawType(e.target.value)}
+          value={drawType}
+        >
+          <Radio value={1}>框选</Radio>
+          <Radio value={2}>表格</Radio>
+        </Radio.Group>
+        <InputNumber
+          min={1}
+          value={tableCols}
+          onChange={(e) => setTableCols(e)}
+        ></InputNumber>
+        列
+        {/* <Button onClick={move}>移动</Button>
+        <Button type="dashed">删除</Button> */}
       </Control>
       <Content>
-        <canvas id="main" height="300" width="1000"></canvas>
+        <canvas id="main" height="300" width="600"></canvas>
       </Content>
-
     </Box>
-  )
+  );
 }
 
 // https://www.cnblogs.com/rachelch/p/14172947.html
