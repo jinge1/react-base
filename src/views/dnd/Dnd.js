bc5d4cce15b7a6644b727020f4d48da2942f02df
一知七分解 <346959251@qq.com>
2021-11-19 20:06:54 +0800

    文档

--- src/views/dnd/Dnd.js
+++ src/views/dnd/Dnd.js
@@ -4,6 +4,8 @@ import styled from "@emotion/styled";
 import Example from './Example'
 import Step1 from './Step1'
 import Step2 from './Step2'
+// https://react-dnd.github.io/react-dnd/docs/overview
+// https://github.com/teleport/react-dnd-treeview/blob/master/README.md
 function DndDemo() {
   return (
     <DndProvider backend={HTML5Backend}>
