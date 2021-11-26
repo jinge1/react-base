6095be37ee6f3087357b482c2d7a9555bbd97090
一知七分解 <346959251@qq.com>
2021-11-26 19:57:07 +0800

    新增组件

--- src/views/about/About.js
+++ src/views/about/About.js
@@ -1,4 +1,48 @@
+import styled from "@emotion/styled";
+import { useState, useCallback } from "react";
+import { ReactSortable } from "react-sortablejs";
+
+const Box = styled.div`
+  line-height: 30px;
+  background: gray;
+`;
+
+const conf = {
+  group: "shared", // set both lists to same group
+  animation: 150,
+};
+
 function About() {
-  return <div>About</div>;
+  const [state1, setState1] = useState([
+    { id: 1, name: "fiona1" },
+    { id: 2, name: "fiona2" },
+    { id: 3, name: "fiona3" },
+    { id: 4, name: "fiona4" },
+    { id: 5, name: "fiona5" },
+  ]);
+  const [state2, setState2] = useState([
+    { id: 21, name: "shrek21" },
+    { id: 22, name: "shrek22" },
+    { id: 23, name: "shrek23" },
+    { id: 24, name: "shrek24" },
+    { id: 25, name: "shrek25" },
+  ]);
+  // const changeList = useCallback((arr) => {
+  //   setState(arr);
+  // }, []);
+  return (
+    <>
+      <ReactSortable list={state1} setList={setState1} {...conf}>
+        {state1.map((item) => (
+          <Box key={item.id}>{item.name}</Box>
+        ))}
+      </ReactSortable>
+      <ReactSortable list={state2} setList={setState2} {...conf}>
+        {state2.map((item) => (
+          <Box key={item.id}>{item.name}</Box>
+        ))}
+      </ReactSortable>
+    </>
+  );
 }
 export default About;
--- src/views/product/Product.js
+++ src/views/product/Product.js
@@ -1,4 +1,101 @@
+import { Button } from "antd";
+import Generator from "fr-generator";
+const { Provider, Sidebar, Canvas, Settings } = Generator;
+
+const defaultValue = {
+  type: "object",
+  properties: {
+    inputName: {
+      title: "简单输入框",
+      type: "string",
+    },
+  },
+};
+
+const settings = [
+  {
+    title: "个人信息",
+    widgets: [
+      {
+        text: "计数器",
+        name: "asyncSelect",
+        schema: {
+          title: "计数器",
+          type: "number",
+          widget: "NewWidget",
+        },
+        setting: {
+          api: { title: "api", type: "string" },
+        },
+      },
+      {
+        text: "姓名",
+        name: "name",
+        schema: {
+          title: "输入框",
+          type: "string",
+        },
+        setting: {
+          maxLength: { title: "最长字数", type: "number" },
+        },
+      },
+      {
+        text: "object",
+        name: "object",
+        schema: {
+          title: "对象",
+          type: "object",
+          properties: {},
+        },
+        setting: {},
+      },
+      {
+        text: "array",
+        name: "array",
+        schema: {
+          title: "数组",
+          type: "array",
+          items: {
+            type: "object",
+            properties: {},
+          },
+        },
+        setting: {},
+      },
+    ],
+  },
+];
+
+const NewWidget = ({ value = 0, onChange }) => (
+  <Button onClick={() => onChange(value + 1)}>{value}</Button>
+);
+
 function Product() {
-  return <div>Product</div>;
+  return (
+    <div style={{ height: "80vh" }}>
+      <Provider
+        onChange={(data) => console.log("data:change", data)}
+        onSchemaChange={(schema) => console.log("schema:change", schema)}
+        widgets={{ NewWidget }}
+        settings={settings}
+      >
+        <div className="fr-generator-container">
+          <Sidebar fixedName />
+          <Canvas />
+          <Settings />
+        </div>
+      </Provider>
+
+      <Generator
+        widgets={{ NewWidget }}
+        commonSettings={{
+          description: {
+            title: "自定义共通用的入参",
+            type: "string",
+          },
+        }}
+      />
+    </div>
+  );
 }
 export default Product;
